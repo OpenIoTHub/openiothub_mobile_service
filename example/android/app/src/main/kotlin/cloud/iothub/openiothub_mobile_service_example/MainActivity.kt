8e9b572c382395fd162db090f2fef89ba15a431c
iotserv <newfarry@126.com>
2023-09-13 20:51:01 +0800

    init

---
+++ example/android/app/src/main/kotlin/cloud/iothub/openiothub_mobile_service_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package cloud.iothub.openiothub_mobile_service_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:openiothub_mobile_service/openiothub_mobile_service.dart' as openiothub_mobile_service;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late int sumResult;
+
+  @override
+  void initState() {
+    super.initState();
+    openiothub_mobile_service.run();
+    sumResult = 3;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 25);
+    const spacerSmall = SizedBox(height: 10);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Native Packages'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                const Text(
+                  'This calls a native function through FFI that is shipped as source in the package. '
+                  'The native code is built as part of the Flutter Runner build.',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+                Text(
+                  'sum(1, 2) = $sumResult',
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+                spacerSmall,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
